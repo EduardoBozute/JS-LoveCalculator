86bad51a2282a7ea56c0f1f1b5630063630bd2ec
Eduardo Bozute <eduardobozutedev@gmail.com>
2025-03-10 11:35:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var you = (prompt("What's your name?"));
 var crush = (prompt("What's your crush's name?"));
 function getRandom(min, max){
-return Math.floor(((Math.random()))*(max-min+1)+ min);
+    var loveScore = Math.random()*100;
+    loveScore = Math.floor(loveScore) + 1;
 }
-alert( you + ", Your score is " + getRandom(1, 100) + " points with " + crush + "." );
+
+if (loveScore > 70) {
+    alert("Amazing! " + you + ", Your score is " + getRandom(1, 100) + " points with " + crush + "." );
+} else {
+    alert(you + ", Your score is " + getRandom(1, 100) + " points with " + crush + "." );
+}
